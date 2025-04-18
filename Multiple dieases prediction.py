62d3d65305f6900cb8eacb3f40e56e3ad6b83afa
Suvendu Khuntia <152652606+Sp019@users.noreply.github.com>
2025-04-18 15:12:15 +0530

    Add files via upload

---
+++ Multiple dieases prediction.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pickle # use for load the saved models
+import streamlit as st #
+from streamlit_option_menu import option_menu #use to create site bar
+
+
